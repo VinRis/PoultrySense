e0d3d007b47bda5f99b305075af618c50f7eb178
Kevin Kariuki <kevinkaris47@gmail.com>
2026-02-12 14:42:10 +0000

    When I hover over this account icon, the icon disappears. (_for element

--- src/app/components/UserNav.tsx
+++ src/app/components/UserNav.tsx
@@ -56,7 +56,7 @@ export function UserNav() {
             <Button variant="ghost" className="relative h-8 w-8 rounded-full">
               <Avatar className="h-8 w-8">
                 <AvatarImage src={user.photoURL ?? ""} alt={user.displayName ?? ""} />
-                <AvatarFallback>
+                <AvatarFallback className="text-muted-foreground">
                   <UserIcon className="h-4 w-4" />
                 </AvatarFallback>
               </Avatar>
