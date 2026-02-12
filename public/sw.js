04d99d4b27150a3a8ff389ad1193f2b0de83c136
Kevin Kariuki <kevinkaris47@gmail.com>
2026-02-12 16:05:37 +0000

    I am getting this when I open the link on my phone.

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,16 @@
-// This is a basic service worker file.
-// It's required for the app to be installable (PWA).
+// A simple, no-op service worker that exists to make the app installable.
 
-self.addEventListener('install', (event) => {
-  // console.log('Service Worker: Installing...');
-  // You can add caching logic here for offline support.
+self.addEventListener('install', () => {
+  // Skip waiting, so the new service worker activates immediately.
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  // console.log('Service Worker: Activating...');
+  // Take control of all clients as soon as the service worker activates.
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  // For now, we just pass the request through to the network.
-  event.respondWith(fetch(event.request));
+  // Do nothing. The browser will handle all network requests as usual.
+  return;
 });
