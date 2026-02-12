d64296de54afba8ab5c33e3e0c7df765fcfb8f23
Kevin Kariuki <kevinkaris47@gmail.com>
2026-02-12 14:35:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9221933085-3816b",
+  "appId": "1:808970307593:web:3e77b991616c9c6755d0d8",
+  "apiKey": "AIzaSyAu8lP3oMDaeU1NzIFn7-kX9yUUvRk7PG0",
+  "authDomain": "studio-9221933085-3816b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "808970307593"
+};
